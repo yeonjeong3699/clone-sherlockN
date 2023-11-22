5b58ebff0cac6c7f018a16e2d5b6e390ef5dfe5d
SYJ <syj3699_@naver.com>
2023-11-22 14:49:15 +0900

    section02  구현 완료

--- script/main.js
+++ script/main.js
@@ -1,9 +1,43 @@
+//sec02-container
+//top
+let $topRoller = $(".rolling-top-box");
+$topRoller.attr("id", "topOriginal");
+
+let topClone = $topRoller.clone(true);
+topClone.attr("id", "topClone");
+
+console.log(topClone)
+
+$(".rolling-top-wrapper").append(topClone);
+
+// $("#topOriginal").css({ "left": "0px" });
+// $("#topClone").css({ "left": $topRoller.outerWidth() + 'px' })
+
+$topRoller.addClass("top-original");
+topClone.addClass("top-clone");
+
+//bottom
+let $bottomRoller = $(".rolling-bottom-box");
+$bottomRoller.attr("id", "bottomOriginal");
+
+let bottomClone = $bottomRoller.clone(true);
+bottomClone.attr("id", "bottomClone");
+
+$(".rolling-bottom-wrapper").append(bottomClone);
+
+// $("#bottomOriginal").css({ "left": "0px" });
+// $("#bottomClone").css({ "left": $bottomRoller.outerWidth() + 'px' })
+
+$bottomRoller.addClass("bottom-original");
+bottomClone.addClass("bottom-clone");
+
+
 //sec03-container
-$(".qna > li").each(function(){
-    $(this).click(function(){
-        if($(this).children().hasClass("on")){
+$(".qna > li").each(function () {
+    $(this).click(function () {
+        if ($(this).children().hasClass("on")) {
             $(this).children().removeClass("on");
-        }else{
+        } else {
             $(this).children().addClass("on");
             $(this).siblings().children().removeClass("on");
         }
